6e0789bcbf9b5203c87e6ebfc3b8f632ff814eb2
allenli <ww90053@gmail.com>
2023-06-30 18:32:06 +0800

    修正主程式呼叫demo.py的路徑 原本為 EX: './input/abcd-0000-0000-0001' 改成 EX: './input/abcd-0000-0000-0001/'

--- main.py
+++ main.py
@@ -32,14 +32,14 @@ def Main():
     print(sys.argv[1])
 
     # 拼接 輸入檔路徑資料夾, EX: './input/abcd-0000-0000-0001/'
-    inputFileDir = os.path.join(DATA_PATH, GUID)
+    inputFileDir = os.path.join(DATA_PATH, GUID)+'/'
     # 如果 輸入檔路徑資料夾不存在,後面不處理了
     if (not os.path.isdir(inputFileDir)):
         print(f'{inputFileDir} not exist')
         print(f'error')
         return False
     # 拼接 輸出檔路徑資料夾, EX: './output/abcd-0000-0000-0001/'
-    outFileDir = os.path.join(PROCESSED_DIR, GUID)
+    outFileDir = os.path.join(PROCESSED_DIR, GUID)+'/'
     # 建立 輸出檔路徑資料夾
     if (not os.path.isdir(outFileDir)):
         os.mkdir(outFileDir)
